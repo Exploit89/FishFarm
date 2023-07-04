963155d1625678e5b3b5ff247e227f7fda1a9dfd
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2023-07-04 17:36:29 +1000

    PC_Work commit
    work with loading circle UI

---
+++ Assets/UsedResources/UI/LoadingCircle.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingCircle : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer.sprite.textureRect.
+    }
+}
